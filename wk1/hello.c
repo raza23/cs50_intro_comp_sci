2429fd43f2a7003bba40ca946c972b9d1d93fdf5
Raza <rtshareef@gmail.com>
2020-04-30 06:52:47 -0400

    week2

---
+++ wk1/hello.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world");
+}
+
+// void cough(int n);
+
+// int main(void)
+// {
+//     cough(3);
+// }
+
+// // Cough some number of times
+// void cough(int n)
+// {
+//     for (int i = 0; i < n; i++)
+//     {
+//         printf("cough\n");
+//     }
+// }
